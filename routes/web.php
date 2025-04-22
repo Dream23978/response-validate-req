e7b3c449408b9c2f5fe3346741d9b6150abce8b0
Dream23978 <ferdian.ff1@gmail.com>
2025-04-22 10:44:55 +0700

    nambah rOUTE

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\http\Controllers\ContactController;
 Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
 Route::post('/user/store', [UserController::class, 'store']) ->name('user.store');
 
+Route::get('/', [UserController::class, 'create']) ->name('user.create');
 Route::get('/contact/create', [ContactController::class, 'create'])->name('contact.create');
 Route::post('/contact/store', [ContactController::class, 'store']) ->name('contact.store');
 
